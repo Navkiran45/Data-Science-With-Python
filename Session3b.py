6ac403da1ab9720e15a220daaca55ae24953071a
Navkiran Kaur <navkirank45@gmail.com>
2024-06-11 01:25:10 +0530

    Session 3

---
+++ Session3b.py
@@ -0,0 +1,39 @@
+# QUANTATIVE APTITUDE QUESTIONS
+
+print("PROBLEM 1")
+# A train running at the speed of 60 km/hr crosses a pole in 9 sec. Find length of train
+speed = 60
+time = 9
+speed = speed * (5/18)
+length = speed * time
+print("Length of train is:" ,length)
+print("----------------------------")
+
+print("PROBLEM 2")
+# A person buys a pen from a wholesaler at Rs. 10 for 20 pens. 
+# He sells those pens at Rs. 10 for 15 pens. Find his profit or loss percent.
+cost_price = 10 / 20
+selling_price =  10 / 15
+profit = selling_price - cost_price
+print("Profit is:" ,profit)
+profit_percent = (profit / cost_price) * 100
+print("Profit percentage is: ", profit_percent)
+print("----------------------------")
+
+print("PROBLEM 3")
+# Find the SI on rupees 10000 deposited for 3 years at 5% per annum.
+P = 10000
+T = 3
+R = 5
+SI = (P*R*T) / 100
+print("Simple Interest is:" , SI)
+print("----------------------------")
+
+print("PROBLEM 4")
+# How long does a train 1000 m long moving at a speed of 90 km/hr would take to pass through a 500 m long bridge?
+d = 1500
+speed = 90
+speed = speed * (5/18) 
+time = d / speed
+print("Time required is: ", time, "sec")
+print("----------------------------")
